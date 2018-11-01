246e92179f51802d0b1b2bc65000ddbe5b0596b2
Nadin <elhe@i.ua>
2018-11-01 10:53:47 +0200

    add server

---
+++ Day_3/Proget_server/models/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Users = require('./user');
+
+module.exports.getById = (paramsId) => Users.findById({"_id": paramsId});
+  
+module.exports.add = function (data) {
+    let User = new Users({
+        login: data.login,
+        email: data.email,
+        password: data.password
+    });
+  
+    return User.save();
+  };
+  
